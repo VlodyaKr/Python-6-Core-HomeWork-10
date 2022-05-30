119f1ee42fdcd6c071284f82da4d443db8e4d26e
Volodymyr Kravchenko <vlodya@gmail.com>
2022-05-30 15:33:16 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from collections import UserDict
+
+
+class AddressBook(UserDict):
+    pass
+
+
+class Record:
+    pass
+
+
+class Field:
+    pass
+
+
+class Name:
+    def __init__(self, name):
+        self.name = name
+
+
+class Phone:
+    pass
+
+
+if __name__ == "__main__":
+    pass
