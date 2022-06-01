fd4f6381dde5c22989c3bd4b9df6323b056e7b00
Volodymyr Kravchenko <vlodya@gmail.com>
2022-06-01 11:24:38 +0300

    Version 0.1.0

---
+++ assistant.py
@@ -0,0 +1,157 @@
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def __str__(self) -> str:
+        return f'{self.value}'
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    pass
+
+
+class Record:
+    def __init__(self, name: Name, phones=[]) -> None:
+        self.name = name
+        self.phone_list = phones
+
+    def __str__(self) -> str:
+        return f'User {self.name} - Phones: {self.phone_list}'
+
+    def add_phone(self, phone: str) -> None:
+        self.phone_list.append(phone)
+
+    def del_phone(self, phone: str) -> None:
+        self.phone_list.remove(phone)
+
+    def edit_phone(self, phone: str, new_phone: str) -> None:
+        self.phone_list.remove(phone)
+        self.phone_list.append(new_phone)
+
+
+class AddressBook(UserDict):
+    def add_record(self, record: Record) -> None:
+        self.data[record.name.value] = record
+
+
+class PhoneUserAlreadyExists(Exception):
+    """You cannot add an existing phone number to a user"""
+
+
+class InputError:
+    def __init__(self, func) -> None:
+        self.func = func
+
+    def __call__(self, contacts, *args):
+        try:
+            return self.func(contacts, *args)
+        except IndexError:
+            return 'Error! Give me name and phone please!'
+        except KeyError:
+            return 'Error! User not found!'
+        except ValueError:
+            return 'Error! Phone number is incorrect!'
+        except PhoneUserAlreadyExists:
+            return 'Error! You cannot add an existing phone number to a user'
+
+
+def salute(*args):
+    return 'Hello! How can I help you?'
+
+
+@InputError
+def add_contact(contacts, *args):
+    name = Name(args[0])
+    phone = Phone(args[1])
+    verify_phone(phone.value)
+    if name.value in contacts:
+        if phone.value in contacts[name.value].phone_list:
+            raise PhoneUserAlreadyExists
+        else:
+            contacts[name.value].add_phone(phone.value)
+            return f'Add phone {phone} to user {name}'
+    else:
+        contacts[name.value] = Record(name, [phone.value])
+        return f'Add user {name} with phone number {phone}'
+
+
+@InputError
+def change_contact(contacts, *args):
+    name, old_phone, new_phone = args[0], args[1], args[2]
+    verify_phone(new_phone)
+    contacts[name].edit_phone(old_phone, new_phone)
+    return f'Change to user {name} phone number from {old_phone} to {new_phone}'
+
+
+@InputError
+def show_phone(contacts, *args):
+    name = args[0]
+    phone = contacts[name].phone_list
+    return f'User {name} - Phones: {phone}'
+
+
+def show_all(contacts, *args):
+    result = 'List of all users:'
+    for key in contacts:
+        result += f'\n{contacts[key]}'
+    return result
+
+
+def goodbye(*args):
+    return 'Good bye!'
+
+
+def unknown_command(*args):
+    return 'Unknown command! Enter again!'
+
+
+def verify_phone(phone: str):
+    new_phone = phone.removeprefix('+').replace('(', '').replace(')', '').replace('-', '')
+    return str(int(new_phone))
+
+
+def help_me(*args):
+    return """Command format:
+    help or ? - this help;
+    hello - greeting;
+    add name phone - add user to directory;
+    change name old_phone new_phone - change the user's phone number;
+    phone name - show the user's phone number;
+    show all - show data of all users;
+    good bye or close or exit or . - exit the program"""
+
+
+COMMANDS = {salute: ['hello'], add_contact: ['add'], change_contact: ['change'], show_phone: ['phone'],
+            help_me: ['?', 'help'], show_all: ['show all'], goodbye: ['good bye', 'close', 'exit', '.']}
+
+
+def command_parser(user_command: str) -> (str, list):
+    for key, list_value in COMMANDS.items():
+        for value in list_value:
+            if user_command.lower().startswith(value):
+                args = user_command[len(value):].split()
+                # print(key, args)
+                return key, args
+    else:
+        return unknown_command, []
+
+
+def main():
+    contacts = AddressBook()
+    while True:
+        user_command = input('Enter command >>> ')
+        command, data = command_parser(user_command)
+        print(command(contacts, *data), '\n')
+        if command is goodbye:
+            break
+
+
+if __name__ == '__main__':
+    main()
